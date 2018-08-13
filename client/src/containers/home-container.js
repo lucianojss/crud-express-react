f2096b1a03a8429aded15a0440f1401a0bf0247c
Aurélio Pita <aurelio.pita@blip.pt>
2018-08-13 22:43:35 +0100

    client

--- client/src/containers/home-container.js
+++ client/src/containers/home-container.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import BookCard from '../components/BookCard';
 
 export const HomeContainer = () => (
     <div>
       <h2>Home</h2>
+      <BookCard></BookCard>
     </div>
   );
