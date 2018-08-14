5d1e027103fa0cb4abdb80fb367dd55e88634ac1
Luciano Santos <lucianojss@gmail.com>
2018-08-14 14:16:19 +0100

    remove shit

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import configureStore from './store';
 
@@ -12,5 +11,3 @@ ReactDOM.render(
     </Provider>,
     document.getElementById('root')
 );
-
-registerServiceWorker();
