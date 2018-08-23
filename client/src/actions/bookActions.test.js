5c6f09a3499a11107ac1cdf7fd738398ec891a7f
Luciano Santos <luciano.santos@blip.pt>
2018-08-24 00:17:16 +0100

    first unit test to actions

--- client/src/actions/bookActions.test.js
+++ client/src/actions/bookActions.test.js
@@ -1,9 +1,22 @@
 import * as bookActions from './bookActions';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 describe('#bookActions', () => {
     describe('#newBookAction', () => {
-a        it('should dispatch new state', () => {
-
-        })
+        it('should dispatch new state', () => {
+            const expectedActions = [
+                {
+                    type: 'NEW_BOOK',
+                },
+            ];
+            const store = mockStore({});
+            store.dispatch(bookActions.newBookAction());
+            expect(store.getActions()).toEqual(expectedActions);
+        });
     });
+
 });
